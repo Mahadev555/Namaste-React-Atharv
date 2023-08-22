3d8b6b896a65185c3f702f163e27c7cf2c632f58
Mahadev555 <mahadevmanerikar@gmail.com>
2023-08-22 14:07:20 +0530

    Use effect hook & swiggy Api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,23 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { RestoList } from "../config";
 import RestroCard from "./RestroCard";
 
 
 
-function filterData (searchText,restaurants){
+function filterData(searchText, restaurants) {
 
-  const fliterDAta= restaurants.filter((restaurant) => 
-             restaurant.data.name.includes(searchText));
+  const fliterDAta = restaurants.filter((restaurant) =>
+    restaurant.info.name.includes(searchText));
 
-             return fliterDAta;
+  return fliterDAta;
 }
 
 const Body = () => {
 
   // SearchText is local state var
-  const [restaurants , setRestaurents] = useState(RestoList);
+  const [restaurants, setRestaurents] = useState(RestoList);
   const [searchText, setSearchText] = useState(); // to create state var;
 
+
+  useEffect(() => {
+    //api call
+    getRestaurants()},
+      []
+  );
+
+  async function getRestaurants() { 
+    // using swiggy data api
+      const response = await fetch(" https://www.swiggy.com/mapi/homepage/getCards?lat=18.4845483&lng=73.8440017");
+      const json = await response.json();
+
+      // optional chaning
+      // set data from api
+      setRestaurents(json?.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle?.restaurants)
+       
+  }
 
   return (
     <>
@@ -32,19 +49,16 @@ const Body = () => {
             setSearchText(e.target.value);
           }}
         ></input>
- 
+
         <button className="search-btn" onClick={() => {
           // filter data
-          const data =  filterData(searchText,restaurants);
-
+          const data = filterData(searchText, restaurants);
           // update the state - Restaurents
-           
-            setRestaurents(data)
-          
+          setRestaurents(data)
 
-        }}>
-          Search
-          </button>
+
+        }}>Search
+        </button>
       </div>
 
       <div className="Restro-List">
